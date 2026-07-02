479d9dd88a81f16bcccbcb36daf6ae860cca13ca
VidhitSikri <sikrividhit@gmail.com>
2026-07-03 01:31:57 +0530

    added final work

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './app/routes/AppRoutes';
 import { AuthProvider } from './app/context/AuthContext';
 import { SocketConnectionManager } from './services/socket/SocketConnectionManager';
+import { RateLimitBanner } from './components/RateLimitBanner';
 
 function App() {
   return (
     <AuthProvider>
       <SocketConnectionManager />
+      <RateLimitBanner />
       <BrowserRouter>
         <AppRoutes />
       </BrowserRouter>
